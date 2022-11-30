0abd20a07d77400e45f9d43ef01d6ea11f2964cc
DarkMan209 <118673371+DarkMan209@users.noreply.github.com>
2022-11-29 21:23:46 -0300

    ok

--- TikFollow/App/script.js
+++ TikFollow/App/script.js
@@ -1,7 +1,9 @@
 var cites = [
-    "https://www.tiktok.com/@manusrb_/video/7166699161125997829"
+    "https://www.tiktok.com/@manusrb_/video/7166699161125997829",
+    "https://www.tiktok.com/@nataliaemboaba/video/7161124191746510086"
 ]
 
 var ids = [
-    "7166699161125997829"
+    "7166699161125997829",
+    "7161124191746510086"
 ]
