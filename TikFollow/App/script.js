903c1f3c774004e2501eb8e4850e3f93d6bff208
DarkMan209 <118673371+DarkMan209@users.noreply.github.com>
2022-11-29 21:28:32 -0300

    ok

--- TikFollow/App/script.js
+++ TikFollow/App/script.js
@@ -7,3 +7,9 @@ var ids = [
     "7166699161125997829",
     "7161124191746510086"
 ]
+
+var descriptions = [
+    "O último desse prometo tá ? 💛💚",
+    `✅ Existe um jeito certo pra passar o corretivo, pra valorizar o seu olhar. <a title="maquiagem" target="_blank" href="https://www.tiktok.com/tag/maquiagem?refer=embed">#maquiagem</a> <a title="makeup" target="_blank" href="https://www.tiktok.com/tag/makeup?refer=embed">#makeup</a> <a title="corretivo" target="_blank" href="https://www.tiktok.com/tag/corretivo?refer=embed">#corretivo</a> <a title="olheiras" target="_blank" href="https://www.tiktok.com/tag/olheiras?refer=embed">#olheiras</a> <a title="olhos" target="_blank" href="https://www.tiktok.com/tag/olhos?refer=embed">#olhos</a> <a title="maquiagembrasil" target="_blank" href="https://www.tiktok.com/tag/maquiagembrasil?refer=embed">#maquiagembrasil</a> <a title="beleza" target="_blank" href="https://www.tiktok.com/tag/beleza?refer=embed">#beleza</a> <a title="belezafeminina" target="_blank" href="https://www.tiktok.com/tag/belezafeminina?refer=embed">#belezafeminina</a> <a title="foryou" target="_blank" href="https://www.tiktok.com/tag/foryou?refer=embed">#foryou</a> <a title="fy" target="_blank" href="https://www.tiktok.com/tag/fy?refer=embed">#fy</a>`
+]
+
